1f6eea351cb4907e1609f41ddb55e52274c1cb90
Michael Braley <michael.parker.braley@gmail.com>
2024-03-27 20:29:01 -0500

    Added Compiled Working version of Code Without Semicolon
    At some point I added a semicolon in the wrong point after the 'io::strin()' line, after that was removed, the program compiles correctly.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,14 @@
-use std:io
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new(); 
-    // We learn in the Rust book that values are immutable by default.
-    // In this variable we use mut which sets the variable as mutable.
+    let mut guess = String::new();
 
-    io::stdin();
-        .read_line(&mut guess);
+    io::stdin()
+        .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
---
+++ guessing_game/target/debug/deps/guessing_game-3f07a13181b8d88d.d
@@ -0,0 +1,5 @@
+/home/calerid/Projects/Rust Book/guessing_game/target/debug/deps/guessing_game-3f07a13181b8d88d: src/main.rs
+
+/home/calerid/Projects/Rust Book/guessing_game/target/debug/deps/guessing_game-3f07a13181b8d88d.d: src/main.rs
+
+src/main.rs:
