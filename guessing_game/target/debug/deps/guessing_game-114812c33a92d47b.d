b37a24b4e448a9b2c9c7d02db60dc2c85ab81dbc
Michael Braley <michael.parker.braley@gmail.com>
2024-03-28 20:00:36 -0500

    Updated Rust Compile of Guessing Game'
    Updated some of the guessing game information. It compiles correctly now.

--- guessing_game/target/debug/deps/guessing_game-114812c33a92d47b.d
+++ guessing_game/target/debug/deps/guessing_game-114812c33a92d47b.d
@@ -1,5 +1,5 @@
-/home/calerid/Projects/Rust Book/guessing_game/target/debug/deps/libguessing_game-114812c33a92d47b.rmeta: src/main.rs
+/home/calerid/Projects/The-Rust-Book-Exercises/guessing_game/target/debug/deps/libguessing_game-114812c33a92d47b.rmeta: src/main.rs
 
-/home/calerid/Projects/Rust Book/guessing_game/target/debug/deps/guessing_game-114812c33a92d47b.d: src/main.rs
+/home/calerid/Projects/The-Rust-Book-Exercises/guessing_game/target/debug/deps/guessing_game-114812c33a92d47b.d: src/main.rs
 
 src/main.rs:
