c0c71aa22e8ad5aa831e6ebda6b7d7d0ca1b1f72
Michael Braley <michael.parker.braley@gmail.com>
2024-04-04 22:18:06 -0500

    Guessing Game First Iteration Working
    Was able to get the guessing game working with help from rustfmt. Not sure what I did wrong before :P.

---
+++ guessing_game/target/debug/deps/guessing_game-713de5d17cbb3c24.d
@@ -0,0 +1,5 @@
+/home/calerid/Projects/The-Rust-Book-Exercises/guessing_game/target/debug/deps/guessing_game-713de5d17cbb3c24: src/main.rs
+
+/home/calerid/Projects/The-Rust-Book-Exercises/guessing_game/target/debug/deps/guessing_game-713de5d17cbb3c24.d: src/main.rs
+
+src/main.rs:
