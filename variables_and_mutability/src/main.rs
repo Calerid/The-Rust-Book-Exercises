001cea1406d0d2f2c7679531a98d41faeff0d119
Michael Braley <michael.parker.braley@gmail.com>
2024-05-04 23:41:09 -0500

    Added Shadowing and Block Level Scoping Example

--- variables_and_mutability/src/main.rs
+++ variables_and_mutability/src/main.rs
@@ -1,14 +1,33 @@
 fn main() {
-    // We need to declare this variable as mutable. Otherwise the second assignment will fail!
+    /*
+    We need to declare this variable as mutable. Otherwise the second assignment will fail!
+    */
     let mut x = 5;
     println!("The value of x is: {x}");
+
+    /*
+    Since we applied 'mut' to x, we can reuse this variable.
+    */
     x = 6;
     println!("The value of x is: {x}");
 
-    // A constant is immutable by design. 
-    // It cannot gain mutability like a variable declared with let.
-    // This is consistent with other languages such as JavaScript.
+    /*
+    A constant is immutable by design. 
+    It cannot gain mutability like a variable declared with let.
+    This is consistent with other languages such as JavaScript.
+    */
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{THREE_HOURS_IN_SECONDS}");
+
+    /*
+    Shadwing and block level scoping.
+    */
+
+    {
+        let x = x * 2;
+        println!("The value of the x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
     
-    println!("{THREE_HOURS_IN_SECONDS}")
 }
